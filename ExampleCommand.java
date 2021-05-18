b9d44676cb004c575e8292755365119d2ae571dc
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 12:08:52 -0400

    n

--- ExampleCommand.java
+++ ExampleCommand.java
@@ -6,15 +6,15 @@ public class ExampleCommand extends GCommand {
         new ColorAdapter(),
         new Track("rotations", 0),
         GDef.bind(state -> new TurnInPlace(100, "left")),
-        GDef.bind(state -> new MapUpdate<>(
+        GDef.bind(state -> new AbstractMap.SimpleEntry<>(
           "is-blue", state.get("color/value").close_to(255, 0, 0)
         )),
-        G.when("is_blue", state -> new MapUpdate<>(
+        G.when("is_blue", state -> new AbstractMap.SimpleEntry<>(
           "mem/rotations", ++state.get("mem/rotations")
         )),
-        GDef.bind(state -> new MapUpdate<>(
+        GDef.bind(state -> new AbstractMap.SimpleEntry<>(
           "given/complete", state.get("mem/rotations") >= 10
-        ))
+        )),
       )
     );
   }
