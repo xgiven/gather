71645b614a3f35987daa219d2c55e3140ba5cae5
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 14:29:09 -0400

    Update SpinWithColor.java

--- examples/SpinWithColor.java
+++ examples/SpinWithColor.java
@@ -8,13 +8,13 @@ public class ExampleCommand extends GCommand {
         new Track("rotations", 0),
         G.until_completion(new TurnInPlace(drive, 100, "left")),
         GDef.bind(state -> new AbstractMap.SimpleEntry<>(
-          "is-blue", state.get("color/value").close_to(255, 0, 0)
+          "is-blue", state.get("color/value").get().close_to(255, 0, 0)
         )),
         G.when("is_blue", state -> new AbstractMap.SimpleEntry<>(
-          "mem/rotations", ++state.get("mem/rotations")
+          "mem/rotations", ++state.get("mem/rotations").get()
         )),
         GDef.bind(state -> new AbstractMap.SimpleEntry<>(
-          "given/complete", state.get("mem/rotations") >= n_rotations
+          "given/complete", state.get("mem/rotations").get() >= n_rotations
         )),
       )
     );
