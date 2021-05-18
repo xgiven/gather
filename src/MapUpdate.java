c261c179fdd8cd31d212d0f940df8751bbd8681f
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 11:51:34 -0400

    nvm

--- src/MapUpdate.java
+++ src/MapUpdate.java
@@ -1,4 +1,4 @@
-public static class MapUpdate<K, V> extends Object implements Map.Entry<K, V>, UnaryOperator<? extends Map<K, V>> {
+public static class MapUpdate<K, V> extends Object implements Map.Entry<K, V>, UnaryOperator<? extends Map<K, V>>, GCommand {
   final K key;
   final V value;
   final Map.Entry<? extends K, ? extends V> entry;
@@ -25,13 +25,7 @@ public static class MapUpdate<K, V> extends Object implements Map.Entry<K, V>, U
   public int hashCode() { return Objects.hash(this.key, this.value); }
   public boolean equals(Object o) {
     if (o instanceof MapUpdate<? extends K, ? extends V>) {
-      return this.key.equals(o.key) && this.value.equals(o.value); 
-    } else if (o instanceof MapUpdate<? super K, ? extends V>) {
-      return o.key.equals(this.key) && this.value.equals(o.value);
-    } else if (o instanceof MapUpdate<? extends K, ? super V>) {
-      return this.key.equals(o.key) && o.value.equals(this.value);
-    } else if (o instanceof MapUpdate<? super K, ? super V>) {
-      return o.key.equals(this.key) && o.value.equals(this.value);
+      return this.key.equals(o.key) && this.value.equals(o.value);
     } else { return false; }
   }
   
