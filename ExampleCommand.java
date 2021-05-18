62fee1f6549c548e203fb158e553fdd41b947f20
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 09:49:29 -0400

    Create ExampleCommand.java

---
+++ ExampleCommand.java
@@ -0,0 +1,20 @@
+public class ExampleCommand extends GCommandObject {
+  public ExampleCommand() {
+    super(
+      GCommand.unit()
+        .comp(new DrivetrainAdapter())
+        .comp(new ColorAdapter())
+        .comp(new Track("rotations", 0))
+        .bind(state -> new TurnInPlace(1, "left"))
+        .bind(state -> new AbstractMap.SimpleEntry<>(
+          "is-blue", state.get("color/value").close_to(255, 0, 0)
+        ))
+        .when("is-blue", state -> new AbstractMap.SimpleEntry<>(
+          "mem/rotations", ++state.get("mem/rotations")
+        ))
+        .bind(state -> new AbstractMap.SimpleEntry<>(
+          "given/complete", state.get("mem/rotations") >= 10
+        ))
+    );
+  }
+}
