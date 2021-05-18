985d1128b55b99519d73ebce8b2bd77ade5308ab
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 10:32:11 -0400

    Update MapUpdate.java

--- src/MapUpdate.java
+++ src/MapUpdate.java
@@ -1,1 +1,38 @@
-
+public static class MapUpdate<K, V> extends Object implements Map.Entry<K, V> {
+  final K key;
+  final V value;
+  final Map.Entry<? extends K, ? extends V> entry;
+  
+  public MapUpdate(K key, V value) {
+    this.key = key;
+    this.value = value;
+    this.entry = null;
+  }
+  
+  public MapUpdate(Map.Entry<? extends K, ? extends V> entry) {
+    this.key = entry.getKey();
+    this.value = entry.getValue();
+    this.entry = entry;
+  }
+  
+  public K getKey() { return this.key; }
+  public V getValue() { return this.value; }
+  
+  public int hashCode() { return Objects.hash(this.key, this.value); }
+  public boolean equals(Object o) {
+    if (o instanceof MapUpdate<? extends K, ? extends V>) {
+      return o.key.equals(this.key) && o.value.equals(this.value);
+    } else { return false; }
+  }
+  
+  public V setValue(V value) {
+    if (!this.entry) {
+      throw new UnsupportedOperationException("Cannot mutate a MapUpdate<K, V>");
+      return null;
+    } else {
+      V return_value = this.entry.setValue(value);
+      this.value = this.entry.getValue();
+      return return_value;
+    }
+  }
+}
