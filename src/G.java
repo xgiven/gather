a6348b4cbba640339ca53b66c016cce6f3ce233e
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 16:02:53 -0400

    begin

--- src/G.java
+++ src/G.java
@@ -1,1 +1,12 @@
-
+public class G {
+  public static comp(UnaryOperator<Map<String, Accessible<Object>>>... ops) {
+    return ops.stream().reduce(
+      GDef.unit(),
+      (a, x) -> a.andThen(x)
+    );
+  }
+  
+  public static comp(UnaryOperator<Map<String, Accessible<Object>>>[] ops) {
+    return G.comp(ops);
+  }
+}
