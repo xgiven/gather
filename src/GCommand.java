4c8e01796926300acbc4dbfcf1f6746ddbd1b23b
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 11:45:42 -0400

    wip GCommand

--- src/GCommand.java
+++ src/GCommand.java
@@ -5,6 +5,18 @@ public abstract class GCommand extends CommandBase implements UnaryOperator<GRea
     this.world = world;
   }
   
+  public static GCommand unit() {
+    return new GCommand(
+      UnaryOperator.identity<GReality>()
+    );
+  }
+  
+  public GCommand comp(GCommand other) {
+    return this.world.compose(other.world);
+  }
+  
+  public GCommand bind(Function<GReality, GCommand> fn)
+  
   public GReality apply(GReality given) {
     return this.world.apply(given);
   }
