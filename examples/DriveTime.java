a42df9249e0acdc8d0c7de9bd7041a94ee1c1a7d
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 16:03:22 -0400

    Update DriveTime.java

--- examples/DriveTime.java
+++ examples/DriveTime.java
@@ -2,22 +2,21 @@ public class DriveTime extends GCommand {
   public DriveTime(int time, DriveSubsystem drive) {
     addRequirements(drive);
     super(
-      GDef.comp(
-        new DriveAdapter(drive),
-        new Track("time-start", null),
-        G.until_completion(new DriveForward(drive, 100, "left")),
+        GDef.bind(_ -> new DriveAdapter(drive)),
+        G.until_completion(_ -> new DriveForward(drive, 100, "left"))
+        GDef.bind(_ -> new Track("time-start", null)),
+        GDef.bind(_ -> new TimeTrack()),
         G.if_equal("given/phase", "__init__",
           state -> new AbstractMap.SimpleEntry<>(
-            "mem/time-start", Timer.getFPGATimestamp()
+            "mem/time-start", state.get("time.value").get()
           )
-        )
+        ),
         GDef.bind(state -> new AbstractMap.SimpleEntry<>(
-          "time-elapsed", state.get("given/time").get() - state.get("mem/time-start").get()
+          "time-elapsed", state.get("time/value").get() - state.get("mem/time-start").get()
         )),
         GDef.bind(state -> new AbstractMap.SimpleEntry<>(
           "given/complete", state.get("time-elapsed").get() >= time
         )),
-      )
     );
   }
 }
