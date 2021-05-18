34205470388eba9bab89a052993e45b3501726ed
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 14:54:40 -0400

    Update GDef.java

--- src/GDef.java
+++ src/GDef.java
@@ -1,5 +1,19 @@
 public class GDef {
   public static UnaryOperator<Map<String, Accessible<Object>>> unit() {
-    return x -> x;
+    return UnaryOperator<>.identity();
+  }
+  
+  public static UnaryOperator<Map<String, Accessible<Object>>> comp(
+    UnaryOperator<Map<String, Accessible<Object>>> f,
+    UnaryOperator<Map<String, Accessible<Object>>> g,
+  ) {
+    return state -> f(g(state));
+  }
+  
+  // Function<Function<UnaryOperator<Map<String, Accessible<Object>>>>, Function<Map<String, Accessible<Object>>, UnaryOperator<Map<String, Accessible<Object>>>>>
+  public static Function<UnaryOperator<Map<String, Accessible<Object>>>> bind(
+    Function<Map<String, Accessible<Object>>, UnaryOperator<Map<String, Accessible<Object>>>> f
+  ) {
+    return state -> null() // TODO
   }
 }
