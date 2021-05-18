f32e799bebcf8fcefa8bb483bc40239dd6f68553
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-18 12:04:35 -0400

    plan

--- ExampleCommand.java
+++ ExampleCommand.java
@@ -1,24 +1,3 @@
-public class ExampleCommand extends GCommandObject {
-  public ExampleCommand() {
-    super(
-      GCommand.unit()
-        .comp(new DrivetrainAdapter())
-        .comp(new ColorAdapter())
-        .comp(new Track("rotations", 0))
-        .bind(state -> new TurnInPlace(1, "left"))
-        .bind(state -> new AbstractMap.SimpleEntry<>(
-          "is-blue", state.get("color/value").close_to(255, 0, 0)
-        ))
-        .when("is-blue", state -> new AbstractMap.SimpleEntry<>(
-          "mem/rotations", ++state.get("mem/rotations")
-        ))
-        .bind(state -> new AbstractMap.SimpleEntry<>(
-          "given/complete", state.get("mem/rotations") >= 10
-        ))
-    );
-  }
-}
-
 public class ExampleCommand extends GCommand {
   public ExampleCommand() {
     super(
@@ -28,12 +7,14 @@ public class ExampleCommand extends GCommand {
         new Track("rotations", 0),
         GDef.bind(state -> new TurnInPlace(100, "left")),
         GDef.bind(state -> new MapUpdate<>(
-          "mem/rotations", ++state.get("mem/rotations")
+          "is-blue", state.get("color/value").close_to(255, 0, 0)
         )),
         G.when("is_blue", state -> new MapUpdate<>(
-          "mem/rotations", state.get("mem/rotations") >= 10
+          "mem/rotations", ++state.get("mem/rotations")
         )),
-        GDef.bind(state -> new MapUpdate)
+        GDef.bind(state -> new MapUpdate<>(
+          "given/complete", state.get("mem/rotations") >= 10
+        ))
       )
     );
   }
