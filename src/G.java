9a6b856cc18764ec6dff88e033f19d1dbb5e4e61
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-19 12:25:55 -0400

    Update G.java

--- src/G.java
+++ src/G.java
@@ -1,12 +1,20 @@
 public class G {
-  public static comp(UnaryOperator<Map<String, Accessible<Object>>>... ops) {
+  public static UnaryOperator<Map<String, Accessible<Object>>> comp(
+    UnaryOperator<Map<String, Accessible<Object>>>... ops
+  ) {
     return base -> ops.stream().reduce(
       base,
       (a, x) -> a.andThen(x)
     );
   }
   
-  public static comp(UnaryOperator<Map<String, Accessible<Object>>>[] ops) {
+  public static UnaryOperator<Map<String, Accessible<Object>>> comp(
+    UnaryOperator<Map<String, Accessible<Object>>>[] ops
+  ) {
     return G.comp(ops);
   }
+  
+  public static UnaryOperator<UnaryOperator<Map<String, Accessible<Object>>>> during(
+    ...
+  )
 }
